3a57deca66f948874ed1e47fb16a039a608f9fd2
Costa <2022320574@aluno.osorio.ifrs.edu.br>
2024-12-12 02:16:57 -0300

    subi o site

---
+++ js/posts.js
@@ -0,0 +1,65 @@
+var posts = [
+  {
+    imagem: "../assets/escorpiãoAmarelo3menor.jpg",
+    titulo: "Escorpião Amarelo",
+    autor: "Betina Braga Santiago",
+    data: "10 Dez 2024",
+    imagem_post: "../assets/escorpiãoAmarelo3.jpg",
+    texto:
+      " ESCORPIÃO AMARELO: Conhecido popularmente como Escorpião-Amarelo, o Tityus serrulatus é uma das espécies de escorpião frequentemente encontrados no sudeste, centro-oeste e nordeste do Brasil. É o maior causador de acidentes no nosso país, os sintomas mais comuns, principalmente em crianças, são vômitos e diarréias, queda de pressão, sonolência, dor abdominal, náuseas, além de deixar a região infectada avermelhada e inchada. O seu veneno pode reagir no nosso corpo com dores intensas e, em casos mais graves, levar a óbito. Sabe-se que a canela em pó atua como um repelente natural de escorpiões, podendo ser espalhada pela casa. Essa espécie apresenta coloração amarelada nas pernas, palpos e cauda, mede cerca de 7 centímetros e possui manchas castanho-escuras no seu dorso. Tem uma visão pouco desenvolvida mas é compensada por pelos sensoriais que auxiliam na localização das suas presas.  Sendo um animal de hábitos noturnos, o escorpião amarelo vive em ambientes escuros, quentes e úmidos. Na região urbana é facilmente encontrado em ambientes com entulhos, redes de esgoto, tubulações de água e de energia, como o lixo que atrai baratas que servem de alimentos para os aracnídeos. Entretanto também pode se abrigar em sapatos, roupas, camas, etc.A sua reprodução ocorre através de partenogêneses - tipo de reprodução assexuada em que o embrião se desenvolve de um óvulo sem ocorrência de fecundação - e são gerados cerca de vinte filhotes, desenvolvidos dentro do corpo feminino. Ao longo da sua vida cada fêmea pode gerar em volta de 160 filhotes. É recomendado jogar água sanitária semanalmente nos ralos da sua casa, para evitar a presença destes animais. INIMIGOS: O escorpião-amarelo tem como inimigos naturais algumas aves, répteis, anfíbios e algumas espécies de aranhas. Temos como exemplares: lacraias, sapos, gaviões, corujas, aranhas, macacos, lagartos, galinhas e camundongos, entre outros.Mas o seu principal predador é o sapo-cururu, ou sapo boi, muito comum nas regiões sul e sudeste do Brasil. O sapo é resistente ao veneno do escorpião e, para comprovar este fato, cientistas aplicaram uma dose da substância equivalente a dez escorpiões no animal, o sapo além de não apresentar qualquer efeito ainda conseguiu se alimentar normalmente de algumas baratas.",
+  },
+  {
+    imagem: "../assets/escorpiãoMarromMenor.jpg",
+    titulo: "Escorpião Marrom",
+    autor: "Laryiza Maria",
+    data: "02 Dez 2024",
+    imagem_post: "../assets/escorpiãoMarromMaior.jpg",
+    texto:
+      "ESCORPIÃO MARROM: O Tityus Bahiensis, ou escorpião-marrom como é mais conhecido, é membro do grupo Aracnídeos - uma classe de animais invertebrados de ordem Scorpiones. O escorpião-marrom tem coloração alaranjada e patas castanhas, as principais formas de identificação dessa espécie são feitas através da mancha preta aparente no último segmento do pedipalpo e pela ausência de serras em sua cauda. Esses escorpiões são encontrados no leste e centro do Brasil. É uma das espécies mais comuns em acidentes juntamente com o escorpião amarelo (Tityus Serrulatus), mesmo que seu veneno seja menos potente que os Tityus Serrulatus, a picada pode levar a óbito em casos mais graves em crianças, idosos e pessoas com baixa imunidade , e outros sintomas como muita dor no local, febre, sudorese, dispnéia. Seu tratamento é sintomático utilizando-se soro antiescorpiônico para sua melhora.Ao contrário de alguns escorpiões o Tityus Bahiensis tem reprodução sexuada onde ocorre a transferência do espermatozóide do macho para a fêmea, a gestação dura em torno de 3 meses, são realizados dois partos por ano com aproximadamente 20 filhotes, podem medir entre 5 a 7 centímetros e o seu tempo de vida é de três a quatro anos. São animais noturnos, durante o dia se escondem da luz, e a noite se acasalam e saem para caçar presas. Preferem ambientes mais úmidos, como a Mata Atlântica. Quanto ao seu comportamento, em geral não é agressivo, mas é notável quando a fêmea está grávida ou quando carrega seu filhotes nas costas que quando perturbadas alertam seus predadores balançando sua cauda, já os machos a princípio fogem mas se persistirem, eles não hesitam em picar.Eles são animais carnívoros que se alimentam de baratas, aranhas, grilos, entre outros.",
+  },
+  {
+    imagem: "../assets/escorpiãoRoboMenor.jpg",
+    titulo: "Escorpião Robô",
+    autor: "Betina Braga Santiago",
+    data: "30 Nov 2024",
+    imagem_post: "../assets/escorpiãoRoboMaior.jpg",
+    texto:
+      "ESCORPIÃO ROBÔ: Outra tecnologia inspirada nesses artrópodes extremamente interessantes foi desenvolvida no Japão, pela companhia japonesa Toshiba. Em que criaram um robô em forma de escorpião para captar imagens no interior dos reatores da antiga Central Nuclear de Fukushima. A missão desse robô-escorpião é proporcionar a localização de objetos caídos e as circunstâncias das vias de acesso à câmera principal. O dispositivo mede 54 centímetros de comprimento e nove centímetros de altura e largura, dobrado possui 26 centímetros, possibilitando se encolher o suficiente para passar por espaços pequenos. Ao dobrar sua cauda ele eleva uma câmera acoplada, oferecendo aos cientistas uma visão segura sobre a localização do combustível nuclear. O robô é controlado por controles similares aos de vídeo games e foi desenvolvido para suportar até dez horas de radiação.",
+  },
+  {
+    imagem: "../assets/escorpiãoPretodaAmazoniaMenor.jpg",
+    titulo: "Escorpião Preto da Amazônia",
+    autor: "Bethina Silvestro",
+    data: "26 Nov 2024",
+    imagem_post: "../assets/escorpiãoPretodaAmazoniaMaior.jpg",
+    texto:
+      "ESCORPIÃO PRETO DA AMAZÔNIA: O escorpião-preto-da-Amazônia (Tityus obscurus), quando está na fase adulta, possui coloração preta e pode chegar até 9 cm de comprimento. Quando jovens, a coloração é bem diferente, com o corpo e apêndices castanhos e com manchas escuras. Essa espécie ocorre somente na região Norte do país, sendo um grande causador de acidentes entre os moradores, a sua picada é extremamente dolorosa causando febre quase instantânea. É considerada a espécie mais venenosa já existente na atualidade. É um animal que segue os padrões de vida dos demais, têm preferência por lugares escuros e úmidos, com disponibilidade de alimentos (insetos) e calmos. O primeiro lar dos escorpiões são as costas da mãe. Os escorpiões são capazes de trocar de pele, um processo denominado ecdise, passando por um número limitado de trocas até a maturidade sexual. Isso ocorre com as demais espécies também. Apesar de pequenos, estes escorpiões fazem uma aranha caranguejeira parecer inofensiva em termos de veneno. Entretanto, galinhas, sapos, camundongos, corujas e algumas aranhas ainda servem como predadores naturais do escorpião",
+  },
+  {
+    imagem: "../assets/escorpiãoAmarelodoNordesteMenor.jpg",
+    titulo: "Escorpião Amarelo do Nordeste",
+    autor: "Bethina Silvestro",
+    data: "18 Nov 2024",
+    imagem_post: "../assets/escorpiãoAmarelodoNordesteMaior.jpg",
+    texto:
+      "ESCORPIÃO AMARELO DO NORDESTE: O escorpião-do-nordeste (Tityus stigmurus) é uma espécie de escorpião comum na Região Nordeste do Brasil.  Tem coloração de camuflagem amarelada, para se confundir com o solo arenoso das regiões áridas em que habita. É facilmente identificado por possuir um triângulo de coloração negra na sua parte dorsal, acompanhado de uma faixa, de coloração também escura, ao longo do corpo. Mede em torno dos 7 cm de comprimento e se alimenta principalmente de insetos, aranhas e outros escorpiões. Como a maioria dos escorpiões, têm hábitos noturnos, sua visão é péssima e ele se orienta por pêlos sensoriais, espalhados por todo o corpo, que percebem vibrações tanto do solo quanto do ar. A espécie é adaptada à vida em ambiente urbano e pode coabitar com seres humanos atraída pela presença de insetos e pela facilidade de abrigo.A picada é dolorosa e necessita de cuidados médicos especialmente em crianças, idosos, pessoas com algum quadro de baixa imunidade ou fragilizadas por outras doenças. Vivem em ambientes úmidos e quentes, como casas de barro e madeira acumulada.  Estes animais  reproduzem-se quase que em sua totalidade por partenogênese, ou seja, não há a necessidade de um macho para acasalar. Apesar disso, existem machos e fêmeas, mas o aparecimento de machos é raro. As fêmeas dão à luz em média de 8 a 14 filhotes. Desta forma podemos observar um aumento relevante na população dos escorpiões, principalmente no meio urbano.Assim como a maioria dos demais escorpiões, este se alimenta de insetos como baratas e grilos, muito encontrados nos entulhos de lixo das cidades, por isso é muito comum o aparecimento destes animais por lá. Devemos sempre tomar cuidados com ambientes escuros e úmidos, principalmente perto das nossas casas. Os escorpiões do nordeste também possuem apreço por lugares calmos sem a presença de predadores como aves, répteis, anfíbios e aranhas",
+  },
+  {
+    imagem: "../assets/escudoBionicoMenor.jpg",
+    titulo: "Escudo Bionico",
+    autor: "Laryiza Maria",
+    data: "05 Nov 2024",
+    imagem_post: "../assets/escudoBionicoMaior.jpg",
+    texto:
+      "ESCUDO BIÔNICO: Na China foi desenvolvido por Han Zhiwu e seus colegas da Universidade Jilin uma forma de proteger máquinas e motores do desgaste. Inspirando-se na espécie escorpião do deserto (Androctonus australis) em que usam um  ''escudo biônico'' natural para se proteger  de tempestades de areia do deserto.  Foi desenvolvido uma carapaça para proteger os equipamentos mecânicos da sua principal causa de  danos e falhas: a erosão por partículas sólidas. Por isso ter uma superfície mais resistente à erosão pode ser a solução mais simples e econômica na maioria dos casos. Então foi usado pelos cientistas um scanner a laser 3D para fazer um mapa de alta precisão da superfície da carapaça do escorpião, que contém muitas microtexturas. E foi feita uma simulação para ver como a areia impacta a carapaça. Após coletar esses dados, desenvolveram diversas estruturas rugosas, e começaram a desenvolver os testes práticos. ''Os resultados mostraram que superfícies microtexturizadas apresentam melhor resistência à abrasão do que as superfícies lisas'', escreveu o grupo. Foi concluído então que uma série de pequenos sulcos em um ângulo de 30 graus em relação ao fluxo de partículas dá a superfícies de aço a melhor proteção contra a erosão.",
+  },
+  {
+    imagem: "../assets/nanotecnologiaMenor.jpg",
+    titulo: "NanoTecnologia",
+    autor: "Betina Braga Santiago",
+    data: "24 Out 2024",
+    imagem_post: "../assets/nanotecnologiaMaior.jpg",
+    texto:
+      "NANOTECNOLOGIA INSPIRADA NO VENENO DOS ESCORPIÕES: O veneno dos escorpiões é uma substância muito estudada no ramo da ciência e principalmente na medicina, por possuir proteínas complexas com diversas funcionalidades. A Scorpion IT iniciou um projeto em pró do tratamento contra o câncer tendo como base a estrutura desta toxina. Após a coleta de células T do sangue do paciente há a modificação da mesma geneticamente a fim de reconhecer o tipo de câncer que o indivíduo porta, desta forma a célula T, agora denominada célula receptora de antígenos quiméricos, é reimplantada na corrente sanguínea do paciente, a fim de atacar o câncer. Desenvolvemos a partir disto um método de tratamento com a utilização do veneno do escorpião. Nos laboratórios da Scorpion IT já estão sendo produzidas quantidades de uma substância pura, contendo somente as proteínas mencionadas anteriormente, assim como outros elementos que servirão de auxílio extra que terá como destino salas médicas para iniciar uma nova forma de tratamento. Com a implantação desta nova substância nas correntes sanguíneas do paciente esperamos que atinja o objetivo de atacar o câncer, seja ele maligno ou benigno, e em poucos meses já haja uma melhora significativa. Esta nanotecnologia poderá ser aplicada em pessoas de todas as idades e esperamos que mais para a frente, após a realização de todos os testes e confirmação de eficácia, possamos alcançar todos os públicos e fornecer a devida ajuda.",
+  },
+];
